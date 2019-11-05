eb35523c770828f3cc43796cd8605848f7046264
RaphaelLima <raphaelshoty@gmail.com>
2019-11-05 11:13:14 -0300

    adding some simple internationalization

--- src/main/java/com/webServices/rest/restFulWebServices/RestFulWebServicesApplication.java
+++ src/main/java/com/webServices/rest/restFulWebServices/RestFulWebServicesApplication.java
@@ -1,7 +1,14 @@
 package com.webServices.rest.restFulWebServices;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class RestFulWebServicesApplication {
@@ -9,5 +16,23 @@ public class RestFulWebServicesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestFulWebServicesApplication.class, args);
 	}
+	
+	//internationalization 
+	@Bean
+	public LocaleResolver localeResolver() {
+		//SessionLocaleResolver localeResolver = new SessionLocaleResolver(); // this way the request header on the service needs to receive an header telling the locale 
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.ENGLISH);
+		return localeResolver;
+	}
 
+	//the configs below could be expressed on the application properties file. to be used with the solution above
+	
+//	@Bean
+//	public ResourceBundleMessageSource messageSource() {
+//		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+//		messageSource.setBasename("messages");
+//		return messageSource;
+//	}	
+	//internationalization
 }
--- src/main/java/com/webServices/rest/restFulWebServices/helloworld/HelloWorldController.java
+++ src/main/java/com/webServices/rest/restFulWebServices/helloworld/HelloWorldController.java
@@ -1,7 +1,13 @@
 package com.webServices.rest.restFulWebServices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 //controller
 @RestController // annotation to tell spring that this class is going to recieve and respond rest calls
 public class HelloWorldController {
+	
+	@Autowired
+	private MessageSource ms;
 	
 	//GET
 	//URI - /hello-world
@@ -19,7 +28,7 @@ public class HelloWorldController {
 	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")	
 	public String helloWorld() {
 		return "Hello World !";
-	}
+	}	
 	
 	@GetMapping(path = "/hello-world-bean")	
 	public HelloWorldBean helloWorldBean() {
@@ -29,6 +38,21 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world/path-parameter/{parameter}")	
 	public HelloWorldBean helloWorldWithPathVariable(@PathVariable String parameter) {
 		return  new HelloWorldBean(String.format("Hello World do %s", parameter));
+	}
+	
+	// internationalized request
+	//this one needs the ResourceBundleMessageSource to be on the base class of the application
+	@RequestMapping(method = RequestMethod.GET, path = "/hello-world-internationalized")	
+	public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false ) Locale locale) { // Locale as a request header tells the service which message to use otherwise use default
+		return ms.getMessage("good.morning.message", null, locale);
+	}
+	
+	// internationalized request
+	//this one just need to be configured on spring properties and make use of the AcceptHeaderLocaleResolver object on the localeResolver bean
+	
+	@RequestMapping(method = RequestMethod.GET, path = "/hello-world-internationalized-easier")	
+	public String helloWorldInternationalizedEasier() { // Locale as a request header tells the service which message to use otherwise use default
+		return ms.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
 	}
 	
 	
