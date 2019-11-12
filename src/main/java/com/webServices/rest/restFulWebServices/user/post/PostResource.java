9da00e4b9a62f7ede53d868fca428872a8a7281b
RaphaelLima <raphaelshoty@gmail.com>
2019-11-11 22:26:48 -0300

    add some filter rules to Post entity

--- src/main/java/com/webServices/rest/restFulWebServices/user/post/PostResource.java
+++ src/main/java/com/webServices/rest/restFulWebServices/user/post/PostResource.java
@@ -7,6 +7,7 @@ import java.util.Objects;
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import com.webServices.rest.restFulWebServices.exception.PostNotFoundException;
 
 @RestController
@@ -23,9 +27,26 @@ public class PostResource {
 	
 	private PostDaoService service = new PostDaoService();
 	
+//	@GetMapping(path = "/users/{id}/posts")
+//	public List<Post> findAll(@PathVariable Integer id) {
+//		return service.findAllByUser(id);
+//	}
+	
+	
+	//using filtering on the mapping dynamically 
 	@GetMapping(path = "/users/{id}/posts")
-	public List<Post> findAll(@PathVariable Integer id) {
-		return service.findAllByUser(id);
+	public MappingJacksonValue findAllFiltered(@PathVariable Integer id) {
+		
+		List<Post> posts = service.findAllByUser(id);
+		
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("postDate","message","user","id"); // saying what i whant to show on my response
+		
+		FilterProvider filterProvider = new SimpleFilterProvider().addFilter("PostsFilter", filter); // here i specify the filter provider name and add the filters to be applied
+		
+		MappingJacksonValue mappedValues = new MappingJacksonValue(posts);	// here i input the entity to be filtered out on its properties 
+		mappedValues.setFilters(filterProvider); // here i set the filter provider to apply the filter rules
+		
+		return mappedValues;
 	}
 	
 	// here below is the implementation using the hateoas feature 
